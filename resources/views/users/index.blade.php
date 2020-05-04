5b0e60dda3ba3ce3983415814a3c90d69ea2a6cf
Luca <luca-marconi@hotmail.com>
2020-05-04 16:17:04 +0200

    card

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="{{route('home')}}">Home</a>
+    </body>
+</html>
