0adaddf075bc4a514059cad03d30f8607d5ec528
Luca <luca-marconi@hotmail.com>
2020-05-06 16:16:07 +0200

    Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,41 @@
+<footer>
+    <img class="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="">
+
+    <div class="info">
+        <div class="column">
+            <ul>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+            </ul>
+        </div>
+        <div class="column">
+            <ul>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+            </ul>
+        </div>
+        <div class="column">
+            <ul>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+                <li>Info</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="background">
+
+    </div>
+
+</footer>
