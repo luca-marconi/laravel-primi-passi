a21592ecb6cca80c8145ef9d36d98cd2b7fa8b96
Luca <luca-marconi@hotmail.com>
2020-05-07 18:08:06 +0200

    Bonus

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,8 @@
 @php
     $arrayProdotti = config('products');
     $prodotto = $arrayProdotti[$id];
-    // dd($prodotto);
+
+
 @endphp
 
 @extends('layouts.layout')
@@ -11,10 +12,22 @@
 @endsection
 
 @section('mainContent')
+
     <div class="prodotto">
+        @if ($id == 0)
+            <a href="{{route('prodotti.show', $id = 11)}}"><i class="fas fa-chevron-left"></i></a>
+        @else
+            <a href="{{route('prodotti.show', $id - 1)}}"><i class="fas fa-chevron-left"></i></a>
+        @endif
         <h3>{{$prodotto['titolo']}}</h3>
         <img src="{{$prodotto['src-h']}}" alt="">
         <img src="{{$prodotto['src-p']}}" alt="">
         <p>{{$prodotto['descrizione']}}</p>
+        @if ($id == count($arrayProdotti) -1)
+            <a href="{{route('prodotti.show', $id = 0)}}"><i class="fas fa-chevron-right"></i></a>
+        @else
+            <a href="{{route('prodotti.show', $id + 1)}}"><i class="fas fa-chevron-right"></i></a>
+        @endif
     </div>
+
 @endsection
